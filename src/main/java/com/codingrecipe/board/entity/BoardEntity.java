54ce2e3ffb1b3c4379bf4fa7f09aceb5b1819cc0
one031411 <one031411@naver.com>
2023-08-09 17:54:43 +0900

    게시글작성 구현 작업중

---
+++ src/main/java/com/codingrecipe/board/entity/BoardEntity.java
@@ -0,0 +1,32 @@
+package com.codingrecipe.board.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+//DB의 테이블 역할을 하는 클래스
+@Entity
+@Getter
+@Setter
+@Table(name = "board_table")
+public class BoardEntity extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    private Long id;
+
+    @Column(length = 20, nullable = false) // 크기 20, not null
+    private String boardWriter;
+
+    @Column
+    private String boardPass;
+
+    @Column
+    private String boardTitle;
+
+    @Column(length = 500)
+    private String boardContents;
+
+    @Column
+    private int boardHits;
+}
